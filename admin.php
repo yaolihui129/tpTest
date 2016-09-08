0e6230d47e1bdc45222e5cad66ae8e769381abe6
yaolihui129 <yaolihui129@sina.com>
2016-09-08 14:33:54 +0800

    开发环境搭建

---
+++ admin.php
@@ -0,0 +1,10 @@
+<?php
+//确定应用名称Admin
+
+define('APP_NAME','Admin');
+
+//2.确定应用路径
+define('APP_PATH','./Admin/');
+
+//3.引入核心文件
+require './ThinkPHP/ThinkPHP.php';
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+//确定应用名称Home 
+
+define('APP_NAME','Home');
+
+//2.确定应用路径
+define('APP_PATH','./Home/');
+
+//3.引入核心文件
+require './ThinkPHP/ThinkPHP.php';
