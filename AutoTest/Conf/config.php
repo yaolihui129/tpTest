d4bf2859a145924cf452328ac554c33e1d81255c
yaolihui129 <yaolihui129@sina.com>
2016-09-20 21:19:31 +0800

    更新线上环境配置文件

--- AutoTest/Conf/config.php
+++ AutoTest/Conf/config.php
@@ -5,11 +5,11 @@ return array(
 	'TMPL_L_DELIM'=>'<{',//修改左定界符
 	'TMPL_R_DELIM'=>'}>',//修改右定界符
 	'DB_TYPE'=>'mysql',
-	'DB_HOST'=>'127.0.0.1',
-	'DB_NAME'=>'tptest',
-	'DB_USER'=>'root',
-	'DB_PWD'=>'root',
-	'DB_PORT'=>'3307',
+	'DB_HOST'=>'mysql.hostinger.com.hk',
+	'DB_NAME'=>'u336217402_test',
+	'DB_USER'=>'u336217402_yaolh',
+	'DB_PWD'=>'123456',
+	'DB_PORT'=>'3306',
 	'DB_PREFIX'=>'tp_',
 	//'DB_DSN'=>'mysql://root:root@127.0.0.1:3307/tms',
 	//'SHOW_PAGE_TRACE'=>true,// 调试模式
